493a0bba59d44590d42168d1880e06872e0dbbc5
poarlmaximal <79842901+poarlmaximal@users.noreply.github.com>
2025-06-22 14:00:24 +0200

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 const config = {
-    gameFactoryAddress: "0x358fC1392783da3C7fe1D46c9c14d7057C201047" // Replace with your deployed GameFactory contract address
+    gameFactoryAddress: "****** YOUR CONTRACT ADDRESS ******" // Replace with your deployed GameFactory contract address
 };
 
 
